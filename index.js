23d2ddc926b2d7c6b71b4b1cbfec8643aea06a82
Julian Johannesen <julian.johannesen@gmail.com>
2018-02-24 05:04:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,20 +14,19 @@ function distanceTravelledInFeet(begin, end){
   return Math.abs(begin - end) * 264;
 };
 
-//`calculatesFarePrice`: Given the same starting and ending block, return the fare for the customer. 
-//The first four hundred feet are free. 
-//For a distance between 400 and 2000 feet, the price is 2 cents per foot. 
-//Then Scuber charges a flat fare for a distance over 2000 feet and under 2500 feet. 
+//`calculatesFarePrice`: Given the same starting and ending block, return the fare for the customer.
+//The first four hundred feet are free.
+//For a distance between 400 and 2000 feet, the price is 2 cents per foot.
+//Then Scuber charges a flat fare for a distance over 2000 feet and under 2500 feet.
 //Finally Scuber does not allow any rides over 2500 feet - the function returns `'cannot travel that far'` if a ride over 2500 feet is requested.
 function calculatesFarePrice(begin, end){
   var feet = distanceTravelledInFeet(begin, end);
-  var fare;
   if (feet <= 400{
-    return fare = 0;
+    return 0;
   } else if(feet < 2000){
-    return fare = feet * 0.02;
+    return feet * 0.02;
   } else if(feet <= 2500) {
-    return fare = 25.00;
+    return 25.00;
   } else {
     return "cannot travel that far";
   }
