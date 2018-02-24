bae2a6f711c8d2125661c5f527eb80abe046adf6
Julian Johannesen <julian.johannesen@gmail.com>
2018-02-24 04:47:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(end){
+    if(end > 42) {
+      return end - 43 + 1;
+    } else {
+      return 43 - end - 1;
+    }
+}
+
+function distanceFromHqInFeet(end){
+  if(end > 42) {
+    return (end - 43 + 1) * 264;
+  } else {
+    return (43 - end - 1) * 264;
+  }
+};
+
+function distanceTravelledInFeet(begin, end){
+  return Math.abs(begin - end) * 264;
+};
+
+function calculatesFarePrice(begin, end){
+};
